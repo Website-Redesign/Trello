337c393001be537c8e44b61ac093bfa46b8826af
wooseok <wooseok6008@gmail.com>
2024-03-18 22:10:31 +0900

    fix(comment): 댓글 삭제 HTTP method 수정

--- src/main/java/com/example/trello/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/trello/domain/comment/controller/CommentController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +32,7 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PostMapping("/{cardId}/comments/{commentId}")
+    @DeleteMapping("/{cardId}/comments/{commentId}")
     public ResponseEntity<Void> deleteComment(
         @PathVariable Long cardId,
         @PathVariable Long commentId,
