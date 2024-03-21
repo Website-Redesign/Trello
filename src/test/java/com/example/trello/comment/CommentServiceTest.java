7f39e5e8c617f35f9f6f446ae6292eb7371fcbfe
wooseok <wooseok6008@gmail.com>
2024-03-21 20:52:30 +0900

    test(comment): 댓글 수정 테스트

--- src/test/java/com/example/trello/comment/CommentServiceTest.java
+++ src/test/java/com/example/trello/comment/CommentServiceTest.java
@@ -1,7 +1,10 @@
 package com.example.trello.comment;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
@@ -16,6 +19,8 @@ import com.example.trello.domain.comment.service.CommentService;
 import com.example.trello.domain.user.entity.User;
 import com.example.trello.domain.user.entity.UserRoleEnum;
 import com.example.trello.domain.user.service.UserService;
+import com.example.trello.global.exception.CommentNotFoundException;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -24,6 +29,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -72,7 +78,7 @@ class CommentServiceTest {
      * CommentService 에서 test 하지 않음
      * */
     @Nested
-    class FindCommentTest {
+    class CreateCommentTest {
 
         @Test
         @DisplayName("댓글 생성 성공 테스트")
@@ -87,6 +93,49 @@ class CommentServiceTest {
             commentService.createComment(testCardId, commentRequestDto, testUser);
 
             then(commentRepository).should(times(1)).save(any(Comment.class));
+        }
+    }
+
+    @Nested
+    class UpdateCommentTest {
+
+        @Test
+        @DisplayName("전달받은 각 id에 해당하는 댓글이 존재하지 않을 경우, 댓글 수정 실패 테스트")
+        void updateCommentFailureTest() {
+
+            Long testCommentId = 123L;
+
+            CommentRequestDto commentRequestDto = new CommentRequestDto();
+            commentRequestDto.setComment("update comment");
+
+            Comment testComment = new Comment(commentRequestDto.getComment(), testUserId,
+                testCardId,
+                "nickname");
+            ReflectionTestUtils.setField(testComment, "commentId", 4L);
+
+            assertThrows(CommentNotFoundException.class, () -> {
+                commentService.updateComment(testCardId, testCommentId, testUserId,
+                    commentRequestDto);
+            });
+        }
+
+        @Test
+        @DisplayName("댓글 수정 성공 테스트")
+        void updateCommentSuccessTest() {
+
+            CommentRequestDto commentRequestDto = new CommentRequestDto();
+            commentRequestDto.setComment("update comment");
+
+            Comment testComment = new Comment(commentRequestDto.getComment(), testUserId,
+                testCardId, "nickname");
+            ReflectionTestUtils.setField(testComment, "commentId", testCommentId);
+
+            given(commentRepository.findByCardIdAndCommentIdAndUserId(anyLong(), anyLong(),
+                anyLong())).willReturn(Optional.of(testComment));
+
+            commentService.updateComment(testCardId, testCommentId, testUserId, commentRequestDto);
+
+            assertEquals(commentRequestDto.getComment(), testComment.getComment());
         }
     }
 }
