bcd4c333221c35ee9c79a329a89cbfcaf3d73655
ParkDongSeob <bagd0445@gmail.com>
2024-03-22 17:34:48 +0900

    feat(member):RedisConfig 코드 수정

--- src/main/java/com/example/trello/global/config/RedisConfig.java
+++ src/main/java/com/example/trello/global/config/RedisConfig.java
@@ -1,26 +1,44 @@
 package com.example.trello.global.config;
 
-//@EnableRedisRepositories
-//@Configuration
-//public class RedisConfig {
-//
-//    @Value("${spring.data.redis.host}")
-//    private String host;
-//
-//    @Value("${spring.data.redis.port}")
-//    private int port;
-//
-//    @Value("${spring.data.redis.timeout}")
-//    private Long timeout;
-//
-//    @Bean
-//    public RedisConnectionFactory redisConnectionFactory() {
-//        return new LettuceConnectionFactory(host, port);
-//    }
-//
-//    @Bean(name="redisTemplate")
-//    public RedisTemplate<String, Map<String, Boolean>> redisTemplate() {
-//        RedisTemplate<String, Map<String, Boolean>> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(redisConnectionFactory());
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Boolean.class));
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.Map;
+
+@EnableRedisRepositories
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.timeout}")
+    private Long timeout;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean(name="redisTemplate")
+    public RedisTemplate<String, Map<String, Boolean>> redisTemplate() {
+        RedisTemplate<String, Map<String, Boolean>> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Boolean.class));
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Boolean.class));
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
