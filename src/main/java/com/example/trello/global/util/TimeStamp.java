9573f300b6c8f90e0ba6b20e476aabcbfe5cdd0f
RebbitK <andxl9979@gmail.com>
2024-03-22 18:57:30 +0900

    feat(user): 직렬화 애너테이션 추가

--- src/main/java/com/example/trello/global/util/TimeStamp.java
+++ src/main/java/com/example/trello/global/util/TimeStamp.java
@@ -1,5 +1,9 @@
 package com.example.trello.global.util;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -16,11 +20,17 @@ public abstract class TimeStamp {
 
     @Column(updatable = false)
     @CreatedDate
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createAt;
 
     @LastModifiedDate
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime updatedAt;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime deletedAt;
 
     public void delete() {
